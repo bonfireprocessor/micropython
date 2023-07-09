38f423624cf17ea578b4a6ac96a8eb587418e32e
Thomas Hornschuh <thomas@bonfirecpu.eu>
2023-07-10 01:11:23 +0200

    _bonfire_poll_event_hook implemented

--- examples/bonfire/timer_dbg.py
+++ examples/bonfire/timer_dbg.py
@@ -12,13 +12,18 @@ from machine import Timer
 
 def run(flag):
    from time import sleep
+   from sys import stdin
+   import select
 
    t=Timer(-1)
    t.init(period=1000, callback=lambda t: cb(flag))
 
-   for i in range(1000000):
-	pass
-
+   p=select.poll()
+   p.register(stdin,select.POLLIN)
+   print("Wait for keypress")
+   print(p.poll())
+   stdin.read(1)
+   
    t.deinit()
    print("Done")	
 
