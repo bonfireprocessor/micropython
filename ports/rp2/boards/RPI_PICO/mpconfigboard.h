752e69b1fb620aab00faa35c6be3a762e86c9580
Thomas Hornschuh <thomas@bonfirecpu.eu>
2024-01-01 18:05:59 +0100

    Local changes to other boards

--- ports/rp2/boards/RPI_PICO/mpconfigboard.h
+++ ports/rp2/boards/RPI_PICO/mpconfigboard.h
@@ -1,3 +1,5 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+#define MICROPY_HW_ENABLE_UART_REPL (1)
+#define MICROPY_HW_USB_CDC (0)
--- ports/stm32/boards/NUCLEO_F411RE/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_F411RE/mpconfigboard.h
@@ -5,6 +5,8 @@
 #define MICROPY_HW_HAS_FLASH        (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_USB       (1)
+#define MICROPY_HW_USB_FS (1)
 
 // HSE is 8MHz, CPU freq set to 96MHz
 #define MICROPY_HW_CLK_PLLM (8)
